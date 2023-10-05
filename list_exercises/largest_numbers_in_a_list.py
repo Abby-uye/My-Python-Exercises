b2f01aac4b49f69b69d33cb34a2a1f822c0410bf
Ayinlade Sikiru <ayinladesikiru@gmail.com>
2023-10-05 09:25:52 +0100

    Update largest_numbers_in_a_list.py

--- list_exercises/largest_numbers_in_a_list.py
+++ list_exercises/largest_numbers_in_a_list.py
@@ -4,3 +4,6 @@ for item in range(len(numbers)):
     if numbers[item] > largest:
         largest = numbers[item]
 print("The largest number is ", largest)
+
+# good, pls give me another algorithm below
+# leave the one above and write another solution below this comment
