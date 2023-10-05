95af2e26ec1c42af03c07f6ed99f35e92a2106e7
Ayinlade Sikiru <ayinladesikiru@gmail.com>
2023-10-05 09:29:15 +0100

    Update smallest_number_in_a_list.py

--- list_exercises/smallest_number_in_a_list.py
+++ list_exercises/smallest_number_in_a_list.py
@@ -4,3 +4,6 @@ for item in range(len(numbers)):
     if numbers[item] < smallest_number:
         smallest_number = numbers[item]
 print("The largest number is ", smallest_number)
+
+# are u sure i'm your teacher lol,
+# pls rethink this your algorthm and rewrite this code. 10Q
