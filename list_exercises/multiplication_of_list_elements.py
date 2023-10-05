9d877625e64896db24545db196031cc1e899dac6
Ayinlade Sikiru <ayinladesikiru@gmail.com>
2023-10-05 09:27:51 +0100

    Update multiplication_of_list_elements.py

--- list_exercises/multiplication_of_list_elements.py
+++ list_exercises/multiplication_of_list_elements.py
@@ -5,3 +5,6 @@ for item in range(len(numbers)):
     total += numbers[item]
     multiplication_of_elements = total * total
 print(multiplication_of_elements)
+
+# why are you getting the total, and are you sure this result is correct
+# rethink this you algorthm ma
